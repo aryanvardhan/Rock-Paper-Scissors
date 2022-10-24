590e918e6ec8e920e1d52927bd643b948b4f57a6
aryanvardhan <av826@snu.edu.in>
2022-10-24 15:21:39 +0530

    added playRound function

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ function getPlayerChoice () {
   let choice = prompt("Enter Rock, Paper or Scissors");
   return choice.toLowerCase();
 }
+
+function playRound (C, P) {
+  let comp;
+  let player;
+
+  for (let i=0; i<3; i++){
+    if (C == choices[i])
+      comp = i
+    if (P == choices[i])
+      player = i
+  }
+
+
+    if (player == comp)
+      return "Tie! You both chose "+C;
+
+    else if ( ((player + 2) % 3) == comp)
+      return "You win! "+P+" beats "+C;
+
+    else if ( ((player + 1) %3) == comp)
+      return "You lose! "+C+" beats "+P;
+
+    else return "Invalid choice";
+}
