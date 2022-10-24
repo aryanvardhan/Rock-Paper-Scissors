45418a03a1b7620309c7f3f0a00e3d3d70094e19
aryanvardhan <av826@snu.edu.in>
2022-10-24 14:39:15 +0530

    added getPlayerChoice function

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ const choices = ["rock", "paper", "scissors"];
 function getComputerChoice () {
   let x = Math.floor(Math.random() * 3);
   return choices[x];
-}  
+}
+
+function getPlayerChoice () {
+  let choice = prompt("Enter Rock, Paper or Scissors");
+  return choice.toLowerCase();
+}
